1288a523c929065555d03811d59334af51b04293
GNOzebac <magbmail@gmail.com>
2020-06-19 02:42:44 -0500

    Add functions

--- Principal.kt
+++ Principal.kt
@@ -1,26 +1,52 @@
-fun main(args : Array<String>) {
-    val rawString : String = """
-        :: Bienvenido a Recipe Maker ::
+fun main(args: Array<String>) {
+    println("::::Sistema::::")
+    showMenu()
+}
 
+fun showMenu() {
+    val rawString: String = """
+        :: Bienvenido a Recipe Maker ::
         Selecciona la opción deseada
         1. Hacer una receta
         2. Ver mis recetas
         3. Salir
-        
     """.trimIndent()
+    println(rawString)
+    do {
+
+        val response: Int = Integer.valueOf(readLine())
+        when (response) {
+            1 -> makeRecipe()
+            2 -> viewRecipe()
+            3 -> println("Salida...")
+        }
+
+    } while (response != 3)
+
+}
 
 
-    do{
-        println(rawString)
-        print("ingrese un valor")
-        val response : Int = Integer.valueOf(readLine())
+fun makeRecipe() {
+    println(" Seleccione Categoria: ")
+    val categorias = listOf<String>("Agua", "Leche", "Carne", "Verduras", "Frutas", "Cereal", "Huevos", "Aceites")
+    for ((i, cat) in categorias.withIndex()) {
+        println("${i + 1} $cat")
+    }
 
-        when(response){
-            1 -> println ("ingresa tu receta")
-            2 -> println ("Tus recetas son: ")
-            3 -> println ("Salida...")
+    println("0. Salir ")
+    do {
 
+        val response: Int = Integer.valueOf(readLine())
+        when (response) {
+            1 -> println("Opcion")
+            2 -> println("Opcion")
+            3 -> println("Opcion")
+        }
 
-            }
-    }while (response != 3)
+    } while (response != 0)
+    showMenu()
+}
+
+fun viewRecipe() {
+    println("Ver mis recetas")
 }
